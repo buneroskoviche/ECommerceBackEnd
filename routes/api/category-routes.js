f0b1d3508e108d8f7ec692110bb25148810b29ac
Buney <dpkinser93@gmail.com>
2021-07-29 22:51:53 -0600

    set up category delete by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,7 +25,7 @@ router.get('/:id', async (req, res) => {
       include: [{ model: Product }]
     });
     if(!categoryById) {
-      res.status(404).json({ message: "No category with that ID!" })
+      res.status(404).json({ message: "No category with that ID!" });
     }
     res.status(200).json(categoryById.toJSON())
   } catch (e) {
@@ -41,8 +41,19 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const catToDelete = await Category.destroy({
+      where: { id: req.params.id}
+    });
+    if(!catToDelete) {
+      res.status(404).json({ message: "No category with that ID!" });
+    }
+    res.status(200).json(catToDelete);
+  } catch (e) {
+    res.status(500).json(e);
+  }
 });
 
 module.exports = router;
