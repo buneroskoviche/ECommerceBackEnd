0949a291a36830c3d3126d261de45df5c20cbd0d
Buney <dpkinser93@gmail.com>
2021-07-30 17:53:43 -0600

    added Tags delete route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -62,8 +62,19 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagToDelete = await Tag.destroy({
+      where: { id: req.params.id}
+    });
+    if(!tagToDelete) {
+      res.status(404).json({ message: "No tag with that ID!" });
+    }
+    res.status(200).json(tagToDelete);
+  } catch (e) {
+    res.status(500).json(e);
+  }
 });
 
 module.exports = router;
